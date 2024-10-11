9caebaa139d3968fd69b737316e68e2dac59bde1
tilekF <gchncbv@gmail.com>
2024-10-11 14:09:14 +0600

    rewriting the project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import Layout from "./layout/layout.tsx";
+
+const App = () => {
+    return (
+        <div className="App">
+            <Layout/>
+        </div>
+    );
+};
+
+export default App;
